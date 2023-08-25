11c6984fdc09faa25cb5eda9679993c162aca43b
Adi Vardi <adi.vardi@enway.ai>
2023-08-25 14:16:53 +0200

    Revert "add flag enable_gyroscope_bias_compensation"
    This reverts commit 79f20c0e74da12bf41bcb65dfeb3bd1598a9661f.

--- src/parse_cfg_file/parse_livox_lidar_cfg.cpp
+++ src/parse_cfg_file/parse_livox_lidar_cfg.cpp
@@ -114,24 +114,15 @@ bool LivoxLidarConfigParser::ParseUserConfigs(const rapidjson::Document &doc,
           user_config.external_frame_id = std::make_optional(static_cast<std::string>(config["external_frame_id"].GetString()));
         }
       }
-    if (!config.HasMember("enable_gyroscope_bias_compensation")) {
+    }
+    if (!config.HasMember("gyroscope_bias")) {
       user_config.gyroscope_bias = {0.0, 0.0, 0.0};
     } else {
-      bool enable_gyroscope_bias_compensation = config["enable_gyroscope_bias_compensation"].GetBool();
-      if (!enable_gyroscope_bias_compensation) {
+      auto &value = config["gyroscope_bias"];
+      if (!ParseGyroscopeBias(value, user_config.gyroscope_bias)) {
         user_config.gyroscope_bias = {0.0, 0.0, 0.0};
-      } else {
-        if (!config.HasMember("gyroscope_bias")) {
-          std::cout << "Error: 'gyroscope_bias' must be specified if 'enable_gyroscope_bias_compensation' is set to true" << std::endl;
-          return false;
-        }
-        auto &value = config["gyroscope_bias"];
-        if (!ParseGyroscopeBias(value, user_config.gyroscope_bias)) {
-          user_config.gyroscope_bias = {0.0, 0.0, 0.0};
-          std::cout << "failed to parse gyroscope bias parameters, ip: "
-                    << IpNumToString(user_config.handle) << std::endl;
-          return false;
-        }
+        std::cout << "failed to parse gyroscope bias parameters, ip: "
+                  << IpNumToString(user_config.handle) << std::endl;
       }
     }
     if (!config.HasMember("extrinsic_parameter")) {
